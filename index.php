7876e899f56d11382d375b15b112d3aeecfc92d2
yahyamahen <kiky.mahendra21@gmail.com>
2020-12-16 18:31:24 +0700

    index home

--- index.php
+++ index.php
@@ -19,24 +19,7 @@
 
 <body>
    <div class="row">
-      <div class="col-md-2 sidebar">
-         <div class="profile d-flex flex-column">
-            <img class="rounded-circle d-inline-block  mb-3" src=" images/profile.jpg" alt="gambar">
-            <a class="card-link" href="akun.php">Rizqi Yahya Mahendra</a>
-            <p>mahasiswa</p>
-         </div>
-
-         <div class="menu-bar p-2 d-flex flex-column">
-            <p class="mt-2 align-self-center">MENU</p>
-            <ul class="list-group">
-               <li class="list-inline mb-2"><a href="" class=" card-link"><i class="fas fa-at mr-2"></i>Daftar Surat</a></li>
-               <li class="list-inline mb-2"><a href="akun.php" class=" card-link"><i class="far fa-user mr-2"></i>Akun</a></li>
-            </ul>
-
-            <button class="btn btn-outline-primary hub-admin-btn">Hubungi Admin</button>
-
-         </div>
-      </div>
+      <?php require "sidebar.php" ?>
 
       <div class="col-md-10">
          <div class="content">
